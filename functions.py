8bbb07d43e73ff469dec81da1554a607f38d294e
Prokhorova Galina <galinaniko@mts.ru>
2021-06-29 21:48:27 +0300

    [feat] task text

---
+++ functions.py
@@ -0,0 +1,6 @@
+from sys import argv
+script_name, first_param, second_param, third_param = argv
+print("name ", script_name)
+print("Param 1: ", first_param)
+print("Param 2: ", second_param)
+print("Param 3: ", third_param)
