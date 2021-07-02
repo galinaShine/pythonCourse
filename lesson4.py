56aed35706f443518ad6b150d2a51c2e5b7862e1
Prokhorova Galina <galinaniko@mts.ru>
2021-07-02 22:12:48 +0300

    [feat] tasks 2-4

--- lesson4.py
+++ lesson4.py
@@ -6,6 +6,8 @@ print(time.time())
 print(random.random())
 import functions
 
+import math
+
 my_list = [10, 25, 30, 45, 50]
 print(my_list)
 new_list = [el + 2 for el in my_list if el % 2 == 1]
@@ -34,16 +36,41 @@ print(new_my_func(4))
 # Пример исходного списка: [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55].
 # Результат: [12, 44, 4, 10, 78, 123].
 
+my_list = []
+while True:       # ручной ввод
+    numb = int(input('enter numb: '))
+    my_list.append(numb)
+    ans = input('enough? type y/n')
+    if ans == 'y':
+        break
+# my_list = [300, 2, 12, 44, 1, 1, 4, 10, 7, 1, 78, 123, 55] # заданный список
+new_list = [el for ind, el in enumerate(my_list) if my_list[ind] > my_list[ind-1] and ind != 0]
+print(new_list)
 
 
 # 3. Для чисел в пределах от 20 до 240 найти числа, кратные 20 или 21. Необходимо решить задание в одну строку.
 # Подсказка: использовать функцию range() и генератор.
+
+my_list = [el for el in range(20, 241) if el % 20 == 0 or el % 21 == 0]
+print(my_list)
+
 
 # 4. Представлен список чисел. Определить элементы списка, не имеющие повторений.
 # Сформировать итоговый массив чисел, соответствующих требованию. Элементы вывести в порядке их следования в исходном списке.
 # Для выполнения задания обязательно использовать генератор.
 # Пример исходного списка: [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11].
 # Результат: [23, 1, 3, 10, 4, 11]
+
+my_list = []
+while True:       # ручной ввод
+    numb = int(input('enter numb: '))
+    my_list.append(numb)
+    ans = input('enough? type y/n')
+    if ans == 'y':
+        break
+# my_list = [2, 2, 2, 7, 23, 1, 44, 44, 3, 2, 10, 7, 4, 11] # заданный список
+new_list = [el for ind, el in enumerate(my_list) if my_list.count(el) == 1]
+print(new_list)
 
 
 # 5. Реализовать формирование списка, используя функцию range() и возможности генератора.
